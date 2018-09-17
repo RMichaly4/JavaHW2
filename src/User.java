f122fbfe34423dcc1af11ef2a9ed5908b6e4b1e4
RMichaly4 <100rm500@gmail.com>
2018-09-17 14:25:59 +0300

    private List <Address> addresses;!

--- src/User.java
+++ src/User.java
@@ -5,16 +5,16 @@ public class User {
     private String id;
     private String firstName;
     private String lastName;
-    private List <Address> adresses;
+    private List <Address> addresses;
     private String email;
     private List <String> phoneNumbers;
     private List <User> friends;
 
     //конструктор
-    User(String id, String firstName, String lastName, List <String> adresses, String email, List <String> phoneNumbers, List<User> friends) {
+    User(String id, String firstName, String lastName, List <Address> addresses, String email, List <String> phoneNumbers, List<User> friends) {
         this.id = id;
         this.firstName = firstName;
-        this.adresses = adresses;
+        this.addresses = addresses;
         this.email = email;
         this.phoneNumbers = phoneNumbers;
         this.friends = friends;
