5862adf2e622ef57f0a29d3eb61a112ade7fc457
RMichaly4 <100rm500@gmail.com>
2018-09-17 17:20:01 +0300

    v.1.01

--- src/UserService.java
+++ src/UserService.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class UserService {
@@ -5,8 +6,9 @@ public class UserService {
     public static void main(String[] args) {
 
         Address Address01 = new Address("Minsk","Belarus","Kalinovskogo 49-58","220103");
+        Address Address02 = new Address("Minsk","Belarus","Sedykh 41-81","220104");
 
         final String ID = String.valueOf(new Random().nextInt(9999));
-        User User01 = new User("id" + ID,"John", "Smith", Address01, "user1@x.xx", ("+998877");
+        User User01 = new User("id" + ID,"John", "Smith", Arrays.asList(Address01, Address02), "user1@x.xx", Arrays.asList("+998877","+112233"),Arrays.asList(), Arrays.asList("admin","manager"));
     }
 }
